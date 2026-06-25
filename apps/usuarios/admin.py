ff5497fa10955b24b54761cd4b62ec1c63c328d3
guirbarros.dev@gmail.com <guirbarros.dev@gmail.com>
2026-06-25 14:11:26 -0300

    Revert "Merge branch 'main' of https://github.com/astonishiing/Commercial-Management-System"
    This reverts commit df8a6eef6576efa0e2cb0f8b0dda216581773600, reversing
    changes made to eb4646424be39b99445e91c2a415c17f9114a031.

--- apps/usuarios/admin.py
+++ apps/usuarios/admin.py
@@ -2,27 +2,17 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 
-
 @admin.register(Usuario)
 class UsuarioAdmin(UserAdmin):
-    list_display  = ('username', 'email', 'perfil', 'is_active', 'is_staff')
+    list_display  = ('username', 'perfil', 'is_active', 'is_staff')
     list_filter   = ('perfil', 'is_active')
-
-    # Campos exibidos na tela de EDIÇÃO de um usuário existente
-    fieldsets = (
+    fieldsets     = (
         (None, {'fields': ('username', 'password')}),
-        ('Informações Pessoais', {'fields': ('email',)}),
         ('Perfil', {'fields': ('perfil',)}),
         ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
     )
-
-    # Campos exibidos na tela de CRIAÇÃO de um novo usuário
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'perfil'),
-        }),
+        (None, {'classes': ('wide',), 'fields': ('username', 'password1', 'password2', 'perfil')}),
     )
-
-    search_fields = ('username', 'email')
+    search_fields = ('username',)
     ordering      = ('username',)
